ec6cf91a1be859d54b73d71cd21b5e746b07d975
Art Tiz <tizner@mail.ru>
2025-01-04 21:46:51 +0700

    loops

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            System.out.println("Введите путь:");
+            String path = new Scanner(System.in).nextLine();
 
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
 
-        int sum  = firstNumber + secondNumber;
-        int diff = firstNumber - secondNumber;
-        int multi =  firstNumber * secondNumber;
-        double quotient = (double)  firstNumber / secondNumber;
+            if (fileExist) {
+                System.out.println("Путь указан верно");
+                continue;
+            }
+            ;
+            if (isDirectory)
+                System.out.println("Указанный путь является путём к папке");
+            else
+                System.out.println("Указанный файл не существует");
 
-        System.out.println("Сумма чисел: " + sum);
-        System.out.println("Разность чисел: " + diff);
-        System.out.println("Произведение чисел: " + multi);
-        System.out.println("Частное чисел: " + quotient);
 
+        }
 
-    }
-}
+    };
+};
